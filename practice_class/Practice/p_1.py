e689063ceb51089a895ea950b30191ba400d1bbd
Joseph <josephdevops6299@gmail.com>
2025-10-30 10:08:03 +0000

    assignment

--- practice_class/Practice/p_1.py
+++ practice_class/Practice/p_1.py
@@ -1,11 +1,8 @@
-idno1 = 120225
-Name1 = "Joseph"
-Salary1 = 25000
 
 
-idno2 = 68467
-Name2 = "Charan"
-Salary2 = 30000
+idno = 145
+idno = 146
+print(idno)
 
-print(idno1,Name1,Salary1 ,idno2,Name2,Salary2)
-
+pno@no = 125
+print(pno@no)
