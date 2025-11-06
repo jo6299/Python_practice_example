dd6814c4b00eb7ff267b74afe979cae930f6c24a
Joseph <josephdevops6299@gmail.com>
2025-11-06 23:29:21 +0000

    oprators assignments

---
+++ practice_class/operators.py
@@ -0,0 +1,63 @@
+format(10/3)
+print("Sathya">"sathya")
+print(9//2)
+print (10 and 10)
+print (10 and 0)
+print (0 and 10)
+print (0 and 0)
+print (0 or 10)
+
+i=5
+j=6
+i=i+j
+j=j-i
+print(i)
+print(j)
+
+i=2*3+5*6//7-3//2+7*6//3
+j=i-3*4-5*6+7-3*4+7
+i=i+j; j=j-i*3/2; i=i*7/-2+5
+j=j-3*4%7
+print(i) 
+print(j) 
+
+i = 3 * 4 - 6 + 7 // 2 + 4
+j = 3 * 5 - 6 // 7 + 8 % 9 - 3 * 5 + 7
+i = i + j; j = j - i
+print(i) 
+print(j)
+
+a = "sathya"
+b = "Tech"
+c = a+b
+print(c)
+
+#s="output is" +2+3
+#print(s)
+
+s=2+3+5*6+7-3*5//6
+print(s)
+
+b = 5+2-3*5+7/2-3/4>6-5+4*3/9+9-2/3
+print(b) 
+
+i=7
+j=3
+b=i>=j
+print(b) 
+
+print(2+3-5*6+7-8)
+print(5**6)
+print(5*6)
+print(2+4)
+print(2+3.5)
+print((2>3))
+print(12//5+12%5) 
+
+print(3>4 and 3<5)
+
+print(5+3*7-9>4+5*6 and 4<5)
+
+print(4**4)
+
+print(6*2-3/4+4-3//2)
