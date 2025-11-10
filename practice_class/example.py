43fcf760752ec17a52abd10671158ab69b1fade9
Joseph <josephdevops6299@gmail.com>
2025-11-10 07:50:36 +0000

    practice program

---
+++ practice_class/example.py
@@ -0,0 +1,29 @@
+# read 2 floats and sum of the two floats
+
+num1 = input("Enter number 1:")
+num2 = input("Enter number 2:")
+
+a = float(num1)
+b = float(num2)
+
+print(type(a))
+print(type(b))
+
+
+print(a+b)
+
+#read 4 numbers and add 3 floats and minus one integer
+
+nub1 = input("Enter number1 :")
+nub2 = input("Enter number2 :")
+nub3 = input("Enter number3 :")
+
+x = float(nub1)
+y = float(nub2)
+z = int(nub3)
+
+print(type(nub1))
+print(type(nub2))
+print(type(nub3))
+
+print(x+y-z)
